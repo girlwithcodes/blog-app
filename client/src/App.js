193fd34db4989827f979d2de7aebbb2d399a9b24
girlwithcodes <heather.mantooth0@gmail.com>
2021-07-15 10:47:26 -0500

    Adds Postcreate route to App.js; adds state for a toggleFetch variable

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,13 @@ import { Route, Switch } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import  { getPosts } from './services/posts';
 import Home from './screens/Home/Home.jsx';
+import Postcreate from './screens/Create/Create.jsx';
 import './App.css';
 
 function App() {
 
   const [posts, setPosts] = useState([]);
+  const [toggleFetch, setToggleFetch] = useState(false);
 
   useEffect(() => {
     const fetchPosts = async() => {
@@ -15,13 +17,17 @@ function App() {
       console.log(resp);
     }
     fetchPosts();
-  }, []);
+  }, [toggleFetch]);
 
   return (
     <div className="App">
       <Switch>
         <Route exact path="/">
           <Home posts={posts}/>
+        </Route>
+
+        <Route exact path="/create">
+          <Postcreate setToggleFetch={setToggleFetch}/>
         </Route>
       </Switch>
     </div>
