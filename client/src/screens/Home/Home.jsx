8316c230b317c9dc3a869d1aedcd48fa711f2761
jubrannassar <juby.nassar123@gmail.com>
2021-07-15 11:49:02 -0400

    creates homepage and displays the latest posts

--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -1,7 +1,19 @@
-function Home (props) {
+import BlogCard from "../../components/BlogCard/BlogCard.jsx";
+
+function Home({ posts }) {
+  const CARDS = posts
+    .reverse()
+    .map((post, index) =>
+      index < 8 ? (
+        <BlogCard post={post}/>) : null
+    );
+
   return (
-    <h2>Home</h2>
-  )
+    <div className="post-cards">
+      <div className="latest">LATEST</div>
+      <div className="cards">{CARDS}</div>
+    </div>
+  );
 }
 
 export default Home;
