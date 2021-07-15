c05cbb91b57b8f1d80a790cbde462883f3d2315d
mnebot <miguelnbt@icloud.com>
2021-07-15 14:45:59 -0400

    added layout to create feed

--- client/src/screens/Create/Create.jsx
+++ client/src/screens/Create/Create.jsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
 import { Redirect } from "react-router-dom";
-import { createPost } from '../../services/posts'
-//TODO: Add Layout
-import './Create.css'
+import Layout from "../../components/Layout/Layout";
+import { createPost } from "../../services/posts";
+import Layout from "../../components/Layout/Layout";
+import "./Create.css";
 
 const Postcreate = (props) => {
   const [post, setPost] = useState({
@@ -12,41 +13,68 @@ const Postcreate = (props) => {
     author: "",
   });
 
-  const [isCreated, setCreated] = useState(false)
+  const [isCreated, setCreated] = useState(false);
 
   const handleChange = (e) => {
-    const {name, value } = e.target
+    const { name, value } = e.target;
     setPost({
       ...post,
-      [name]:value
-    })
-  }
+      [name]: value,
+    });
+  };
 
   const handSubmit = async (e) => {
-    e.preventDefault()
-    const created = await createPost(post)
-    setCreated({ created })
-    props.setToggleFetch((curr) => !curr)
-    
-  }
+    e.preventDefault();
+    const created = await createPost(post);
+    setCreated({ created });
+    props.setToggleFetch((curr) => !curr);
+  };
 
   if (isCreated) {
-    return <Redirect to={`/`} />
+    return <Redirect to={`/`} />;
   }
 
   return (
-    <form onSubmit={handSubmit}>
-      <label htmlFor="title">Title: </label>
-      <input type="text" name="title" id="title" required autoFocus onChange={handleChange} />
-      <label htmlFor="imgURL">Image: </label>
-      <input type="url" name="imgURL" id="imgURL" placeholder="URL" required onChange={handleChange} />
-      <label htmlFor="content">Content: </label>
-      <textarea type="text" name="content" id="content" required onChange={handleChange}/>
-      <label htmlFor="author">Author: </label>
-      <input type="text" name="author" id="author" required onChange={handleChange}/>
-      <button type="submit">Post It!</button>
-    </form>
+    <Layout>
+      <form onSubmit={handSubmit}>
+        <label htmlFor="title">Title: </label>
+        <input
+          type="text"
+          name="title"
+          id="title"
+          required
+          autoFocus
+          onChange={handleChange}
+        />
+        <label htmlFor="imgURL">Image: </label>
+        <input
+          type="url"
+          name="imgURL"
+          id="imgURL"
+          placeholder="URL"
+          required
+          onChange={handleChange}
+        />
+        <label htmlFor="content">Content: </label>
+        <textarea
+          type="text"
+          name="content"
+          id="content"
+          required
+          onChange={handleChange}
+        />
+        <label htmlFor="author">Author: </label>
+        <input
+          type="text"
+          name="author"
+          id="author"
+          required
+          onChange={handleChange}
+        />
+        <button type="submit">Post It!</button>
+      </form>
+    </Layout>
   );
 };
 
-export default Postcreate
+export default Postcreate;
