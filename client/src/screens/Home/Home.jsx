bd05c018e9be9fffa7f5c5fa56808dd2a7957071
girlwithcodes <heather.mantooth0@gmail.com>
2021-07-16 15:31:33 -0500

    Slight styling chanegs to blog card; style and layout to home screen

--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -1,5 +1,6 @@
 import BlogCard from "../../components/BlogCard/BlogCard.jsx";
 import Layout from "../../components/Layout/Layout.jsx";
+import './Home.css';
 
 function Home({ posts }) {
   const CARDS =
