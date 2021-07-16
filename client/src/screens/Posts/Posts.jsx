07aac5bd930817b91865d0b1bd16e2b2571283f3
jubrannassar <juby.nassar123@gmail.com>
2021-07-16 16:37:23 -0400

    adds css to posts screen

--- client/src/screens/Posts/Posts.jsx
+++ client/src/screens/Posts/Posts.jsx
@@ -1,21 +1,20 @@
-import Layout from '../../components/Layout/Layout.jsx';
-import BlogCard from '../../components/BlogCard/BlogCard.jsx';
+import Layout from "../../components/Layout/Layout.jsx";
+import BlogCard from "../../components/BlogCard/BlogCard.jsx";
+import "./Posts.css";
 
 function Posts(props) {
   const posts = props.posts;
   const CARDS = posts.map((post) => {
-    return (
-      <BlogCard post={post} key={post._id} />
-    )
-  })
+    return <BlogCard post={post} key={post._id} />;
+  });
   return (
     <Layout>
-      <div className = "posts-display">
+      <div className="posts-display">
+        <div className="posts-title">All Posts:</div>
         {CARDS}
       </div>
     </Layout>
-    
-  )
+  );
 }
 
 export default Posts;
