ca7fb794bf6b367bf570a7daa6f13161dbdf30d0
girlwithcodes <heather.mantooth0@gmail.com>
2021-07-16 11:47:18 -0500

    Modifies layout and style of BlogCard component

--- client/src/components/BlogCard/BlogCard.jsx
+++ client/src/components/BlogCard/BlogCard.jsx
@@ -4,7 +4,9 @@ function BlogCard({post}) {
   return (
     <Link to={`/post/${post._id}`}>
       <div key={post._id} className="blog-card">
-        <img src={post.imgURL} alt={post.title} className="blog-card-image"/>
+        <div className="blog-card-image-div">
+          <img src={post.imgURL} alt={post.title} className="blog-card-image"/>
+        </div>
         <div className="blog-card-info">
           <h3 className="blog-card-title">{post.title}</h3>
           <p className="blog-card-author">-{post.author}</p>
